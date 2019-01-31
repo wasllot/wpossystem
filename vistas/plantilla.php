b59253ff20a438395c6c013af2bb9922a1dec0e8
wasllot dev <rei.vzl@gmail.com>
2019-01-30 21:14:03 -0430

    logo

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -16,7 +16,7 @@ session_start();
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-  <link rel="icon" href="vistas/img/plantilla/icono-negro.png">
+  <link rel="icon" href="vistas/img/plantilla/logo.png">
 
    <!--=====================================
   PLUGINS DE CSS
