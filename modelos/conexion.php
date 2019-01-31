bd0d4ed1377868242d4c2b52954c10ee5314bebc
wasllot dev <rei.vzl@gmail.com>
2019-01-30 21:09:25 -0430

    database setup

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=pos",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=sql10.freemysqlhosting.net;dbname=sql10276639",
+			            "sql10276639",
+			            "DC5guFZcx2");
 
 		$link->exec("set names utf8");
 
